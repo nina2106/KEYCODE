43a3b43458c429ebdc590bcb41f24a2713b4b38c
nina2106 <carito2106.saavedra@gmail.com>
2024-08-15 20:09:43 -0500

    add

---
+++ ejercicios/ejercicios.js
@@ -0,0 +1,10 @@
+/// GENERAR LAS TABLAS DE MULTIPLICAR DEL 1 AL 10, SEPARADOS
+
+for (let i = 1; i <= 10; i++) {
+    console.log(`tabla del ${i}`);
+
+    for (let j = 1; j <= 10; j++) {
+        console.log(`${i} x ${j} = ${i * j}`);
+      }
+      console.log(''); 
+}
