becc1a12f24046d63ca1fdb20fe53a11136b6b2f
Carolina Saavedra <carito2106.saavedra@gmail.com>
2024-10-16 19:44:12 -0500

    Caro

--- Practica/testing/src/App.jsx
+++ Practica/testing/src/App.jsx
@@ -2,12 +2,16 @@ import './App.css'
 
 function App() {
    
-  const
+  const handleSum = (a, b) =>{
+    const respuesta = sum(a, b)
+    console.log("Respuesta: ", respuesta)
+  }
 
 
   return (
     <>
-      
+      <h1 testid='h1'>Jest Testing</h1>
+      <button onClick={() => handleSum(5, 5)}>Sumar</button>
     </>
   )
 }
