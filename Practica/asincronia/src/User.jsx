a784476a5cda5c9fd684c5a08349f71cd35bca7f
carolina saavedra <carito2106.saavedra@gmail.com>
2024-10-20 18:53:23 -0500

    Caro

--- Practica/asincronia/src/User.jsx
+++ Practica/asincronia/src/User.jsx
@@ -5,34 +5,46 @@ const User = () => {
   const [cargando, setCargando] = useState(true);
   const [error, setError] = useState(null);
 
+  //Convertimos nuestra funcion en asincrona
   const obtenerUsuarios = async () => {
     try {
-      const respuesta = await fetch("https://jsonplaceholder.typicode.com/users");
+      //Esperamos que la API responda para poder continuar
+      const respuesta = await fetch(
+        "https://jsonplaceholder.typicode.com/users"
+      );
+      //Si la respuesta no es OK obligo que genere un error
       if (!respuesta.ok) {
         throw new Error(`Error en la API: ${respuesta.status}`);
       }
+      //Esperamos a poder convertir la respuesta en json para continuar
       const data = await respuesta.json();
       setUsuarios(data);
+      setCargando(false);
     } catch (error) {
+      //Aqui esta el codigo por si ocurre un error
       setError(error.message);
       console.error("Hubo un error al obtener los usuarios:", error);
     } finally {
-      setCargando(false);
+      setCargando(false); // Nos aseguramos de detener la carga, incluso si hay un error
     }
   };
 
+  //Ejecuta esto una unica vez apenas carga la pagina
   useEffect(() => {
     obtenerUsuarios();
   }, []);
 
+  //Ejecuta esto si aun no hay informacion
   if (cargando) {
     return <p>Cargando...</p>;
   }
 
+  //Ejecuta esto si ocurrio un error
   if (error) {
     return <p>Error: {error}</p>;
   }
 
+  //Ejecuta esto si ya no esta cargando y nuestro proceso de obtener usuarios fue satisfactorio
   return (
     <div>
       <h1>Lista de usuarios</h1>
--- Practica/testing/src/App.jsx
+++ Practica/testing/src/App.jsx
@@ -1,17 +1,22 @@
+
 import './App.css'
+// import { sum } from './utils/sum'
+import Counter from './components/Counter'
 
 function App() {
-   
-  const handleSum = (a, b) =>{
-    const respuesta = sum(a, b)
-    console.log("Respuesta: ", respuesta)
-  }
-
+  
+  // const handleSum = (a, b) => {
+  //   const respuesta = sum(a, b)
+  //   console.log("Respuesta: ", respuesta)
+  // }
 
   return (
     <>
+      
       <h1 testid='h1'>Jest Testing</h1>
-      <button onClick={() => handleSum(5, 5)}>Sumar</button>
+      {/* <button onClick={() => handleSum(5, 5)}>Sumar</button> */}
+      <hr />
+      <Counter/>
     </>
   )
 }
